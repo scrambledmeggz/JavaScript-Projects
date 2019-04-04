667ae206e9c180b77bd6259a3f9e501f279eaba0
Megan Carroll <46762397+scrambledmeggz@users.noreply.github.com>
2019-04-04 14:14:27 -0600

    Update simple-image.js

--- simple-image.js
+++ simple-image.js
@@ -1,18 +1,39 @@
-// write your code here
-var image = new SimpleImage("dinos.png");
-var width = image.getWidth();
-var height = image.getHeight();
+var image = new SimpleImage("duke_blue_devil.png");
 
-for (var pixel of image.values()) {
-    if (pixel.getX() <= width/3) {
-        pixel.setRed(255);
-    }
-    else if (width/3 < pixel.getX() && pixel.getX() <= width*(2/3)) {
-        pixel.setGreen(255);
-    }
-    else {
-        pixel.setBlue(255);
+
+function redGreenBlue(image) {
+    var width = image.getWidth();
+    var height = image.getHeight();
+    for (var pixel of image.values()) {
+        if (pixel.getX() <= width/3) {
+            pixel.setRed(255);
+        }
+        else if (width/3 < pixel.getX() && pixel.getX() <= width*(2/3)) {
+            pixel.setGreen(255);
+        }
+        else {
+            pixel.setBlue(255);
+        }
     }
 }
 
-print(image);
+function swapRedGreen(pixel) {
+    var redVal = pixel.getRed();
+    var greenVal = pixel.getGreen();
+    pixel.setRed(greenVal);
+    pixel.setGreen(redVal);
+    return pixel;
+}
+
+function blueToYellow(image) {
+    var width = image.getWidth();
+    var height = image.getHeight();
+    for (var pixel of image.values()) {
+        if (pixel.getRed() != 255 && pixel.getGreen() != 255 && pixel.getBlue() != 255) {
+            pixel.setRed(255);
+            pixel.setGreen(255);
+            pixel.setBlue(0);
+        }
+    }
+    return image;
+}
